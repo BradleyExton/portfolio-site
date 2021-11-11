44f4269656fd88bf326d43791b6ecac8b19918bc
BradleyExton <bradley@bradleyexton.ca>
2021-11-11 16:27:55 -0500

    Add icon assets

---
+++ src/components/icons/html5.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Html5 = () => (
+  <svg
+    width="39"
+    height="38"
+    viewBox="0 0 39 38"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_17_425)">
+      <g clip-path="url(#clip1_17_425)">
+        <g clip-path="url(#clip2_17_425)">
+          <path
+            d="M9.70128 34.2024L6.57153 0.00119019H40.9598L37.8276 34.1976L23.7437 38L9.70128 34.2024Z"
+            fill="#E44D26"
+          />
+          <path
+            d="M35.1451 32.0198L37.8227 2.79776H23.7656V35.0942L35.1451 32.0198Z"
+            fill="#F16529"
+          />
+          <path
+            d="M23.751 6.992H12.97L14.1314 19.6769H23.7657V15.4826H18.0692L17.6756 11.1874H23.7657V6.992H23.751ZM23.7657 26.3625L23.7474 26.3673L18.9528 25.1061L18.6481 21.7609H14.324L14.9273 28.3468L23.7462 30.7218L23.7657 30.7159V26.3625Z"
+            fill="#EBEBEB"
+          />
+          <path
+            d="M23.751 19.6769H29.0525L28.565 25.1156L23.7632 26.3779V30.742L32.5893 28.3587L33.7715 15.4803H23.7656L23.751 19.6769ZM34.4321 8.11775L34.5345 6.992H23.7485V11.1863H34.1469L34.2322 10.2434L34.4321 8.11775Z"
+            fill="white"
+          />
+        </g>
+      </g>
+    </g>
+    <defs>
+      <clipPath id="clip0_17_425">
+        <rect width="39" height="38" fill="white" />
+      </clipPath>
+      <clipPath id="clip1_17_425">
+        <rect
+          width="39"
+          height="38"
+          fill="white"
+          transform="translate(4.26562)"
+        />
+      </clipPath>
+      <clipPath id="clip2_17_425">
+        <rect
+          width="39"
+          height="38"
+          fill="white"
+          transform="translate(4.26562)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default Html5;
