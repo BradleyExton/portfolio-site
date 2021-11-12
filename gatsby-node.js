ade22474bf7c9f09a7fc65ceaaab299890705b60
BradleyExton <bradley@bradleyexton.ca>
2021-11-11 20:08:57 -0500

    Create GlobalStyles

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       alias: {
         "@components": path.resolve(__dirname, "src/components"),
         "@config": path.resolve(__dirname, "src/config"),
+        "@styles": path.resolve(__dirname, "src/styles"),
       },
     },
   });
